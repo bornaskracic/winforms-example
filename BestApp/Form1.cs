f285e67e54c865fa7788fbdcf9b306e6d07117cf
bornaksracic <bskracic@racunarstvo.hr>
2023-05-15 10:47:22 +0200

    initial commit

---
+++ BestApp/Form1.cs
@@ -0,0 +1,18 @@
+namespace BestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowMessage();
+        }
+
+        private void ShowMessage()
+            => MessageBox.Show(DateTime.Now.ToLongTimeString(), "Data");
+    }
+}
